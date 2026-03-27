621a50dc102803a6786b90ea1e4e56d256c096b7
HGUSonny <slt010916@handong.ac.kr>
2026-03-27 15:03:00 +0900

    ?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import HearingSheet from "./HearingSheet";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/hearing-sheet" element={<HearingSheet />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/hearing-sheet" element={<HearingSheet />} />
+    </Routes>
   );
 }
