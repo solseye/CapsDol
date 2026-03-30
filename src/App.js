3e4a9371d3aacb707273e10daf749cee610aada7
HGUSonny <slt010916@handong.ac.kr>
2026-03-30 12:15:08 +0900

    add reservation page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import HearingSheet from "./HearingSheet";
-import Chat from "./pages/Chat/Chat";
 import ReservationPage from "./pages/ReservationPage";
+// import Chat from "./pages/Chat/Chat"; // chat도 쓰면 이것도 추가
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/hearing-sheet" element={<HearingSheet />} />
-      <Route path="/chat" element={<Chat />} />
       <Route path="/reservation" element={<ReservationPage />} />
+      {/* <Route path="/chat" element={<Chat />} /> */}
     </Routes>
   );
 }
