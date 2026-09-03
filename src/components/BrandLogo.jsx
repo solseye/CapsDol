e3093cd6dc0d67924964e93a5f8e86428e9949fa
rlaalsrl1002 <83528473+rlaalsrl1002@users.noreply.github.com>
2026-09-03 18:19:40 +0900

    Replace site branding with RTJ logo

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/mk-office-logo-transparent.png";
+import logo from "../assets/rtj-logo.png";
 
 export function BrandMark({ className = "", alt = "" }) {
   return (
